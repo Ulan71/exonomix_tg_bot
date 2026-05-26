38522af93c6f28322f6368dae301b20902e898e3
Lakosh <kalimbek.ulan@gmail.com>
2026-05-26 10:15:17 +0500

    app.run

--- economix_back/main.py
+++ economix_back/main.py
@@ -243,5 +243,5 @@ def download_file(session_uuid):
 
 if __name__ == "__main__":
     # app.run(debug=True)
-    app.run(debug=True, threaded=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True, threaded=True)
     # serve(app, host='0.0.0.0', port=5000)
